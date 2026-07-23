21513358ab6f658224e861029a2a6ec4b3128f6b
bgyoo970 <bbap970@gmail.com>
2026-07-23 18:24:43 -0400

    init project works

---
+++ src/main/java/org/fightstick/fightstick/FightStickArtworkApplication.java
@@ -0,0 +1,13 @@
+package org.fightstick.fightstick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightStickArtworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FightStickArtworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fightstick/fightstick/FightStickArtworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fightstick.fightstick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FightStickArtworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
